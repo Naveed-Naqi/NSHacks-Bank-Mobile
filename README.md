a5fe0066fc5c9e460545f5b748edd1c56c59191b
davidy9000 <davidy.9000@gmail.com>
2018-11-17 06:04:53 -0500

    Updated Webpage Files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# NSHacks-Project
+# To The Bank Mobile
+
+Visit website here:
+
+## Authors
+* **Naveed** [Naveed-Naqi](https://github.com/Naveed-Naqi)
+* **Theodore** [TJ4](https://github.com/TJ4)
+* **Vincent** [PokeLegoCuber](https://github.com/PokeLegoCuber)
+* **David** [davidy9000](https://github.com/davidy9000)
+* **Ariq** [ariqzaman](https://github.com/ariqzaman)
--- bankRing.py
+++ bankRing.py
@@ -2,6 +2,8 @@ import matplotlib as plt
 import pandas as pd
 import numpy as py
 import folium
+from folium import plugins
+import seaborn as sns
 import os
 import json
 
@@ -13,7 +15,11 @@ for index,row in cOneLocations.iterrows():
     lon = row["Longitude"]
     address = row["Address"]
     bankNo = row["Bank Num"]
-    newMarker = folium.CircleMarker([lat, lon], popup = address)
+    newMarker = folium.CircleMarker([lat, lon], radius=15, popup = address, fill_color="#3db7e4")
     newMarker.add_to(mapBanksLocations)
+
+bankArr = cOneLocations[['Latitude', 'Longitude']].as_matrix()
+
+mapBanksLocations.add_child(plugins.HeatMap(bankArr, radius=15))
 
 mapBanksLocations.save(outfile = 'bankRings.html')
