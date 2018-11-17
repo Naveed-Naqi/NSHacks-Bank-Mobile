0e099bbcd9403a93716084d0437a5e902b4e12fa
davidy9000 <davidy.9000@gmail.com>
2018-11-16 20:45:10 -0500

    Initial Commit

---
+++ basicmap.py
@@ -0,0 +1,17 @@
+import matplotlib as plt
+import pandas as pd
+import numpy as py
+import folium
+
+capOneLocFile = "Capital_One_Locations.csv"
+cOneLocations = pd.read_csv(capOneLocFile)
+mapBanksLocations = folium.Map(location = [40.76831, -73.964915], tiles="Cartodb Positron", zoom_start=10)
+for index,row in cOneLocations.iterrows():
+    lat = row["Latitude"]
+    lon = row["Longitude"]
+    address = row["Address"]
+    bankNo = row["Bank Num"]
+    newMarker = folium.Marker([lat, lon], popup = address)
+    newMarker.add_to(mapBanksLocations)
+
+mapBanksLocations.save(outfile = 'CaptialOneLocations.html')
